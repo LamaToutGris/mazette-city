5aa8d2de78deb9de08b0206515eeb878b41fe7a6
LamaToutGris <104725633+LamaToutGris@users.noreply.github.com>
2025-02-12 17:08:56 +0100

    Adding shelves

---
+++ https___0ab5dbe0215b567279526ccab0ce5c6c.cdn.bubble.io_f1729954780514x551129897122834400_Mazette.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="https___0ab5dbe0215b567279526ccab0ce5c6c.cdn.bubble.io_f1729954780514x551129897122834400_Mazette" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../Mazette/https___0ab5dbe0215b567279526ccab0ce5c6c.cdn.bubble.io_f1729954780514x551129897122834400_Mazette.png" width="256" height="256"/>
+</tileset>
